12adb572b6335f565e20843dc27fe89c1e5fdde6
Onur Ozer <onurozer@gmail.com>
2014-10-25 14:40:17 +0800

    final version of analysis script

--- run_analysis.R
+++ run_analysis.R
@@ -1,23 +1,21 @@
-# You will be required to submit: 
-#    1) a tidy data set as described below
-#    2) a link to a Github repository with your script for performing the analysis, and 
-#    3) a code book that describes the variables, the data, and any transformations or 
-#    work that you performed to clean up the data called CodeBook.md. 
-
-# A full description is available at the site where the data was obtained:     
-# http://archive.ics.uci.edu/ml/datasets/Human+Activity+Recognition+Using+Smartphones 
-
-# Here are the data for the project: 
-# https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip 
-
-# You should create one R script called run_analysis.R that does the following:
+# This R Script does the following:
 # 1. Merges the training and the test sets to create one data set.
 # 2. Extracts only the measurements on the mean and standard deviation for each measurement. 
 # 3. Uses descriptive activity names to name the activities in the data set
 # 4. Appropriately labels the data set with descriptive variable names. 
 # 5. From the data set in step 4, creates a second, independent tidy data set with the average of each variable for each activity and each subject.
 
-# Please upload your data set as a txt file created with write.table() using row.name=FALSE
+require(reshape2)
+require(data.table)
+
+# check if data directory exist
+if(!file.exists("UCI HAR Dataset")){
+
+    # download and unzip if it doesn't    
+    fileUrl <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
+    download.file(fileUrl, destfile="UCI HAR Dataset.zip", method="curl")
+    unzip("UCI HAR Dataset.zip")
+}
 
 # read activity labels (categories)
 activities <- read.table("./UCI HAR Dataset/activity_labels.txt")
@@ -68,4 +66,9 @@ merged_data <- rbind(test_data, training_data)
 # Uses descriptive activity names to name the activities in the data set
 merged_data$Activity <- factor(merged_data$Activity, levels=activities$V1, labels = activities$V2)
 
+# creates a second, independent tidy data set with the average of each variable for each activity and each subject.
+melted_data <- melt(merged_data, id = c("Subject", "Activity"), measure.vars = colnames(merged_data)[3:length(merged_data)])
+tidy_data <- dcast(melted_data, Subject + Activity ~ variable, mean)
 
+# write data set as a txt file created with write.table() using row.name=FALSE
+write.table(tidy_data, file="./final_data.txt", row.name=FALSE)
